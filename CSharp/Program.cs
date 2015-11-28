8e1ed40162a01f24ea2ff91a4b55ecfd6c9ca86f
why2code <futurefinancesolutions@gmail.com>
2015-11-28 16:13:40 +0200

    Added function to calculate Power of 2 numbers

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using System; /*Kazva na PC da izpolzva tazi biblioteka i neinite vyzmojnosti v programata*/
 
 
 namespace CSharp
 {
-    class Program
+    class HelloCSharp /*Klasyt e edinica, v koqto se namirat parcheta kod. Mojem da gi izpolzvame v drugi blokcheta kod i obiknoveno stoqt
+        v drugi failove. Vseki class ima otdelni blokcheta kod (otdolu).*/
     {
-        static void Main()
+        static void Main() /*absolutno zadyljitelno pole i trqbva da e Main! Tova pokazva na PC nachalnata tochka na koda.*/
         {
 
-            Console.WriteLine("Hello there!");
+            Console.WriteLine("Hello there!"); /*Tuk veche sa otdelnite komandi. Razdelqt se s ; . Tochkata . mejdu Console i Writeline
+            kazva na PC: vzemi konzolata i v neq napishi "Hello there!" */
+
+            /*Otdolu vkarvame 2 chisla v konzolata i gi stepenuvame (chislo A na stepen chislo B).
+            Konzolata chete SAMO TEKST, zatova trqbva da se konvertira v chislo chrez A=int.Parse i vyvedenoto kato tekst v konzolata.*/
+            int A;
+            string NumberOne = Console.ReadLine();
+            A = int.Parse(NumberOne);
+
+            int B;
+            string NumberTwo = Console.ReadLine();
+            B = int.Parse(NumberTwo);
+
+            Console.WriteLine(Math.Pow(A, B));
+
         }
     }
 }
