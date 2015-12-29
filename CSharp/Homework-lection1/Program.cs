cc664dfcb4ded408d00d4689dfff2cf908b32695
why2code <futurefinancesolutions@gmail.com>
2015-12-29 22:53:19 +0200

    Adding latest training C# files on Primitive data types and other basics.

---
+++ CSharp/Homework-lection1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_lection1
+{
+    class PrintNumbers
+    {
+        static void Main()
+        {
+
+            //Writing numbers and my name on separate lines (function \n)
+            Console.WriteLine("1\n100\n1001");
+            Console.WriteLine("Haralampi\nStoyanov");
+
+            //Calculating Square of a number provided by user input
+            int x;
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Sqrt(x));
+
+            /*Problem 9. Print a Sequence
+            Write a program that prints the first 10 members of the sequence: 2, -3, 4, -5, 6, -7, ... */
+
+            //  NE RESHENO USLOVIE 9!
+
+            /* Problem 10. Reformat C# Code
+
+            Reformat the following C# code to make it readable according to the C# best practices for code formatting. 
+            Change the casing of the identifiers in the code (e.g. use PascalCase for the class name): HorribleCode.cs */
+
+             Console.WriteLine("Hi, I am horribly formatted program");
+             Console.WriteLine("Numbers and squares:");
+             for (int i = 0; i < 10; i++)
+             {
+                  Console.WriteLine(i + " --> " + i * i);
+              }
+
+
+            //Problem 14*. Printing current date and time
+
+            Console.WriteLine(DateTime.Now);
+
+            /*roblem 15.* Age after 10 Years
+            Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.*/
+
+            Console.Write("Enter your birth date: ");
+            DateTime BirthDay = DateTime.Parse(Console.ReadLine());
+            DateTime now = DateTime.Now;
+            int age = (int)((now - BirthDay).TotalDays / 365.242199);
+            Console.WriteLine("You are " + age + " year(s) old.");
+            Console.WriteLine("After ten years, you will be at the age of " + (age + 10));
+
+            
+
+
+        }
+}
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*Problem 1. Declare Variables
+            Declare five variables choosing for each of them the most appropriate of the types byte, sbyte, short, ushort, int, uint, long, ulong 
+            to represent the following values: 52130, -115, 4825932, 97, -10000.
+            Choose a large enough type for each number to ensure it will fit in it. Try to compile the code. */
+
+            ushort VarOne = 52130;
+            sbyte VarTwo = -115;
+            int VarThree = 4825932;
+            byte VarFour = 97;
+            short VarFive = -10000;
+
+            /* Problem 2. Float or Double?
+            Which of the following values can be assigned to a variable of type float and which to a variable of type 
+            double: 34.567839023, 12.345, 8923.1234857, 3456.091?
+            Write a program to assign the numbers in variables and print them to ensure no precision is lost. */
+
+            double LongNum = 34.567839023;
+            double SecondLongNum = 8923.1234857;
+            float ShortNum = 12.345f;
+            float SecondShortNum = 3456.091f;
+            string Line = "\n";
+
+            Console.Write(LongNum + Line + SecondLongNum + Line + ShortNum + Line + SecondShortNum);
+
+            /* Problem 3. Variable in Hexadecimal Format
+            Declare an integer variable and assign it with the value 254 in hexadecimal format (0x##).
+            Use Windows Calculator to find its hexadecimal representation.
+            Print the variable and ensure that the result is 254. */
+
+
+
+
+        }
+    }
+}
