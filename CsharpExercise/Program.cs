752733344e9ab399448816f12dbbe218b0c40bac
why2code <futurefinancesolutions@gmail.com>
2016-03-31 21:31:16 +0300

    Csharp exercise on new string console drawing.

---
+++ CsharpExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime birthDay = new DateTime(1990, 10, 09);
+            DateTime todaysDay = DateTime.Now;
+            var MyAge = todaysDay.Year - birthDay.Year;
+            Console.WriteLine(MyAge);
+            var myFutureAge = MyAge + 10;
+            Console.WriteLine(myFutureAge);
+
+
+
+
+
+        }
+    }
+}
